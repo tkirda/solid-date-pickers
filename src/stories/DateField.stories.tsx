d66932f308c6728d7f65ef6ac6aee6cbc74977de
Tomas Kirda <tkirda@gmail.com>
2023-12-11 20:59:01 -0600

    Cleanup storybook

--- src/stories/DateField.stories.tsx
+++ src/stories/DateField.stories.tsx
@@ -1,21 +1,17 @@
 import type { Meta, StoryObj } from "storybook-solidjs";
-
 import DateField from "../DateField";
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/solid/writing-stories/introduction
 const meta = {
     title: "DateField",
     component: DateField,
     tags: ["autodocs"],
-    argTypes: {
-        // backgroundColor: { control: "color" },
-    },
+    argTypes: {},
 } satisfies Meta<typeof DateField>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/7.0/solid/writing-stories/args
 export const Primary: Story = {
     args: {
         format: "YYYY-MM-DD hh:mm AM",
